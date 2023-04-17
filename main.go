02a03de34a01d11fd4cfbe417bc74916362e33d5
Jiss Jose <32099292+jisspala@users.noreply.github.com>
2023-04-17 09:19:44 +0200

    adding the documentation

--- main.go
+++ main.go
@@ -15,16 +15,12 @@ import (
 // @query.collection.format multi
 func main() {
 
-	// os.Setenv("POSTGRES_USER", "user")
-	// os.Setenv("POSTGRES_PASSWORD", "admin")
-	// os.Setenv("POSTGRES_PORT", "5432")
-	// os.Setenv("POSTGRES_HOST", "localhost")
-	// os.Setenv("POSTGRES_DB", "notepad")
-
+	//staring db connection
 	repository.InitDb()
+
 	fmt.Println("Welcome to smartnotes ")
 	router := routes.SetupRouter()
 	router.Run(":8001")
-	docs.SwaggerInfo.Title = "Swagger Example API"
+	docs.SwaggerInfo.Title = "Smartnotes"
 
 }
